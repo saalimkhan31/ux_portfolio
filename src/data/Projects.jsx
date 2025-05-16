3279ceb8c140440ffdc38cf3ab48b68495a76278
Saalim Khan <saalimkhan02@gmail.com>
2025-05-16 17:45:16 +0530

    vercel file added

---
+++ src/data/Projects.jsx
@@ -0,0 +1,16 @@
+import image from "../assets/images/project-1.jpg";
+export const projects = [
+  {
+    id: 1,
+    title: "AI Object Removal",
+    subtitle: "Smart Content Editing Solution",
+    image: { image },
+  },
+  {
+    id: 2,
+    title: "Mobile Fitness App",
+    subtitle: "Personalized Workout Experience",
+    image: { image },
+  },
+  // Add more projects as needed
+];
